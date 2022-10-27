b3857ca96c338e9f30b4a9593b0da094c3e7f7af
AngelaLedo29 <angelaledo29@gmail.com>
2022-10-27 14:14:35 +0200

    Modificar Modelos

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -23,36 +23,28 @@ class Book(models.Model) :
         '''String for representing the Model object.'''
         return self.title
 
-class Genre(models.Model) :
-    '''Model representing a book genre.'''
-    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(max_length=200, 
+        help_text='Enter a book genre (e.g. Science Fiction)')
 
-    def __str__(self) :
-        '''String for representing the Model object.'''
+    def __str__(self):
+        """String for representing the Model object."""
         return self.name
 
-class Language(models.Model) :
-    '''Model representing a Language (e.g. English, French, Japanese, etc.)'''
-    name = models.CharField(max_length=200, help_text='Enter the book\'s natural language (e.g. English, French, Japanese, etc.)')
-
-    def __str__(self) :
-        '''String for representing the Model object.'''
-        return self.name
-    
-class Author(models.Model) :
-    """
-    Modelo que representa un autor
-    """
+class Author(models.Model):
+    """Model representing an author."""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
 
-    def get_absolute_url(self):
-        """
-        Retorna la url para acceder a una instancia particular de un autor.
-        """
-        return reverse('author-detail', args=[str(self.id)])
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
 
 
     def __str__(self):
@@ -60,3 +52,12 @@ class Author(models.Model) :
         String para representar el Objeto Modelo
         """
         return '%s, %s' % (self.last_name, self.first_name)
+
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200, 
+        help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
