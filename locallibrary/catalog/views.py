ecc0cc5ee29e2702689380e9cb263434f06c036d
AngelaLedo29 <angelaledo29@gmail.com>
2022-11-02 10:49:06 +0100

    Modifocar Modelos

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Book
 
 # Create your views here.
 def index(request):
     texto = '''<h1>Librería Local</h1>
     <p>Esta es la página principal de la librería local.</p>'''
     # texto = 'Página inicial de la librería local'
-    return HttpResponse(texto)
+    lista = '<h2>Mi lista de ultimos libros</h2><ul>'
+    # Colsulta a la base de datos: primero los 5 libros
+    #for libro in Book.objects.all()[:5]:
+    #   lista += f'<li>{libro.title} ({libro.author})</li>'
+    # Consulta a la base de datos: últimos 5 libros
+    for libro in Book.objects.all().order_by('-id')[:5]:
+        lista += f'<li>{libro.title}</li>'
+    lista += '</ul>' # Fuera del bucle
+
+    return HttpResponse(texto + lista)
 
 def acerca_de(request):
     texto = '''<h1>Acerca de</h1>
