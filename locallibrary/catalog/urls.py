102e9182a6cfe6efcd2e9cb80fc15b1f7b2683e3
AngelaLedo29 <angelaledo29@gmail.com>
2022-11-09 09:17:12 +0100

    Creación de las Vistas Genéricas en las URLS

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,9 +1,10 @@
 # Creamos urls.py en la carpeta catalog
 from django.urls import path
-from .views import index, acerca_de
+from .views import index, acerca_de, BookListView
 
 urlpatterns = [
     path('', index, name='index'),
     path('acercade/', acerca_de, name='acercade'),
+    path('libros/', BookListView.as_view(), name='libros'),
 ]
 
